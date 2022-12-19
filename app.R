f529d121cdc33e3ac2e505d42198071107e65d7d
Hao <110519127+haoo01@users.noreply.github.com>
2022-12-19 01:47:29 -0500

    organized final

--- app.R
+++ app.R
@@ -15,6 +15,7 @@ library(DT)
 library(data.table)
 library(RColorBrewer)
 library(gepaf)
+library(osrm)
 
 ## load files for shiny
 
@@ -28,7 +29,7 @@ may<- read_csv("may.csv", show_col_types = FALSE)
 # this helps me separate ferry and commuter info
 
 # ferry in May
-# boat_trips_may<- may %>% filter(route_desc == "Ferry") %>% distinct(trip_id,.keep_all = T)
+boat_trips_may<- may %>% filter(route_desc == "Ferry") %>% distinct(trip_id,.keep_all = T)
 # Boat-Lewis				
 # Boat-Hingham				
 # Boat-Charlestown				
@@ -47,6 +48,8 @@ may<- read_csv("may.csv", show_col_types = FALSE)
 
 ########### 
 ########### BUS
+
+
 sl_routes<- read_csv("sl_routes.csv")
 
 sl_routes_traveltimes<- read_csv("sl_routes_traveltimes.csv")
@@ -56,52 +59,27 @@ sl_routes_traveltimes<- read_csv("sl_routes_traveltimes.csv")
 # load t stops & travel times 
 red_travel_times<- read_csv("red_travel_times.csv", show_col_types = FALSE) 
 orange_travel_times<- read_csv("orange_travel_times.csv", show_col_types = FALSE)
-# blue_travel_times<- read_csv("blue_travel_times.csv", show_col_types = FALSE)
-# greenb_travel_times<- read_csv("greenb_travel_times.csv", show_col_types = FALSE)
-# greend_travel_times<- read_csv("greend_travel_times.csv", show_col_types = FALSE)
-# greene_travel_times<- read_csv("greene_travel_times.csv", show_col_types = FALSE)
-# mattpan_travel_times<- read_csv("mattpan_travel_times.csv", show_col_types = FALSE)
+blue_travel_times<- read_csv("blue_travel_times.csv", show_col_types = FALSE)
+greenb_travel_times<- read_csv("greenb_travel_times.csv", show_col_types = FALSE)
+greend_travel_times<- read_csv("greend_travel_times.csv", show_col_types = FALSE)
+greenc_travel_times<- read_csv("greend_travel_times.csv", show_col_types = FALSE)
+greene_travel_times<- read_csv("greene_travel_times.csv", show_col_types = FALSE)
+mattpan_travel_times<- read_csv("mattpan_travel_times.csv", show_col_types = FALSE)
 
-# ## red line density plot table
-# red_to_north<- red_travel_times %>% filter(direction_id ==1)
-# red_dept_dens_n<- with(red_to_north, tapply(travel_time_sec, INDEX = dept_name, density)) # now this is a nested list contains density travel times grouped by each stop
-# 
-# # Northbound
-# red_to_north_df<- data.frame(
-#   x = unlist(lapply(red_dept_dens_n, "[[", "x")),
-#   y = unlist(lapply(red_dept_dens_n, "[[", "y")),
-#   dept_name = rep(names(red_dept_dens_n), each = length(red_dept_dens_n[[1]]$x))
-# )
-# 
-# # Southbound
-# red_to_south <- red_travel_times %>% filter(direction_id == 0)
-# red_dept_dens_s<- with(red_to_south, tapply(travel_time_sec, INDEX = dept_name, density))
-# red_to_south_df<- data.frame(
-#   x = unlist(lapply(red_dept_dens_s, "[[", "x")),
-#   y = unlist(lapply(red_dept_dens_s, "[[", "y")),
-#   dept_name = rep(names(red_dept_dens_s), each = length(red_dept_dens_s[[1]]$x))
-# )
+red_stops <- read_csv("red_stops.csv",show_col_types = FALSE) %>% arrange(stop_lat)
+orange_stops <- read_csv("orange_stops.csv",show_col_types = FALSE) %>% arrange(stop_lat)
+greenb_stops <- read_csv("greenb_stops.csv",show_col_types = FALSE) %>% arrange(stop_lat)
+greenc_stops <- read_csv("greenc_stops.csv",show_col_types = FALSE) %>% arrange(stop_lat)
+greend_stops <- read_csv("greend_stops.csv",show_col_types = FALSE) %>% arrange(stop_lat)
+greene_stops <- read_csv("greene_stops.csv",show_col_types = FALSE) %>% arrange(stop_lat)
 
-## Orange line density plot table
+blue_stops <- read_csv("blue_stops.csv",show_col_types = FALSE) %>% arrange(stop_lat)
+mattpan_stops <- read_csv("mattpan_stops.csv",show_col_types = FALSE) %>% arrange(stop_lat)
+subwaystopts<- read_csv("subwaystops.csv",show_col_types = FALSE)
 
-orange_to_north<- orange_travel_times %>% filter(direction_id ==1)
-orange_dept_dens_n<- with(orange_to_north, tapply(travel_time_sec, INDEX = dept_name, density)) # now this is a nested list contains density travel times grouped by each stop
-
-# use double bracket to unlist and store the associated density into a dataframe
-orange_to_north_df<- data.frame(
-  x = unlist(lapply(orange_dept_dens_n, "[[", "x")),
-  y = unlist(lapply(orange_dept_dens_n, "[[", "y")),
-  dept_name = rep(names(orange_dept_dens_n), each = length(orange_dept_dens_n[[1]]$x))
-)
-
-
-orange_to_south <- orange_travel_times %>% filter(direction_id == 0)
-orange_dept_dens_s<- with(orange_to_south, tapply(travel_time_sec, INDEX = dept_name, density))
-orange_to_south_df<- data.frame(
-  x = unlist(lapply(orange_dept_dens_s, "[[", "x")),
-  y = unlist(lapply(orange_dept_dens_s, "[[", "y")),
-  dept_name = rep(names(orange_dept_dens_s), each = length(orange_dept_dens_s[[1]]$x))
-)
+# load data for Orange line density plot table
+orange_to_south_df <- read_csv("orange_to_south_df.csv")
+orange_to_nouth_df <- read_csv("orange_to_north_df.csv")
 
 
 ###########-------------------
@@ -112,14 +90,14 @@ orange_to_south_df<- data.frame(
 # #### Distance Matrix
 #  obtain a matrix of driving distance and duration between 2 locations
 # 
-# locations = c("South Station", "Kenmore")
+# locations = c("A", "B")
 # doc = mp_matrix(
 #   origins = locations,
 #   destinations = locations,
-#   # transit_mode =
-#   # arrival_time = The desired time of arrival for transit directions, as POSIXct
-#   # departure_time =
-#   # avoid = "ferries" or "indoor"
+#   # transit_mode,
+#   # arrival_time = The desired time of arrival for transit directions, as POSIXct,
+#   # departure_time,
+#   # avoid = "ferries" or "indoor",
 #   key = api_key,
 #   quiet = TRUE
 # )
@@ -129,7 +107,7 @@ orange_to_south_df<- data.frame(
 ##D   origins = locations,
 ##D   destinations = locations,
 ##D   mode = "transit",
-##D   departure_time = Sys.time() + as.difftime(10, units = "mins"),
+##D   transit_mode = 
 ##D   key = key
 ##D )
 ##D mp_get_matrix(doc, value = "distance_m")
@@ -139,6 +117,9 @@ orange_to_south_df<- data.frame(
 ##D 
 
 
+# # set up api_key first
+api_key = "AIzaSyAZZEueevQyJrIRJUregAEbUC775LEfrxg"
+
 ################### 
 ########################## UI
 ui <- fluidPage(
@@ -147,31 +128,36 @@ ui <- fluidPage(
     "MBTA Transit Services",
     id = "main_navbar",
     theme = "cosmo",
-    tabPanel("MBTA", 
-             fluidRow(column(4,selectInput("Tstop1","Origin", choices = red_travel_times$dept_name, selected = NULL)),
-                    column(4,selectInput("Tstop2","Destination", choices = red_travel_times$dept_name, selected = NULL))),         
-             fluidRow(leafletOutput("map"),textOutput("intro"),height = 700))
-                 ,
+    tabPanel("MBTA", leafletOutput("map"),textOutput("intro"),height = 700)
+    ,
     tabPanel(
       "Buses",
-      fluidRow(column(width = 3,selectInput(inputId = "bus_stop", label = "bus stop", choices = unique(sl_routes_traveltimes$stop_name),selected = NULL))
-               )
+      fluidRow(column(width = 6,selectInput(inputId = "bus_stop", label = "Bus stop", choices = unique(sl_routes_traveltimes$stop_name),selected = NULL))
+      )
       ,fluidRow(column(width = 12, DTOutput("bus_table")))
-      ),
-    tabPanel("Ferry",
-      # Tab: Ferry
-      fluidRow(column(width = 3,selectInput(inputId = "boat_stop", label = "ferry stop", choices = unique(boat_trips_may$stop_name),selected = NULL))
-      ),
-      fluidRow(column(width = 12, DTOutput("boat_table")))),
+    ),
+    tabPanel(
+      "Ferry",
+      fluidRow(column(width = 6,selectInput(inputId = "boat_stop", label = "Ferry stop", choices = unique(boat_trips_may$stop_name),selected = NULL))
+      )
+      ,fluidRow(column(width = 12, DTOutput("boat_table")))
+    ),
+    tabPanel("Rapid Transit", 
+             sidebarLayout(
+               sidebarPanel(
+                 fluidRow(column(4,selectizeInput("Tstop1",label = "Origin", choices = red_stops$stop_name, selected = NULL,options = list(placeholder = 'select a T station name'))),
+                 column(4,selectizeInput("Tstop2","Destination", choices =red_stops$stop_name , selected = NULL, options = list(placeholder = 'select a T station name')))),
+                 ),
+               mainPanel(leafletOutput("subwaymap")))
+    ),
     tabPanel("Rapid Transit EDA", 
-           sidebarLayout(
-             sidebarPanel(
-               selectInput("lines", "Orange Line:", choices = c("Northbound","Southbound"), selected = NULL)
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("lines", "Orange Line:", choices = c("Northbound","Southbound"), selected = NULL)
                ),
-             mainPanel(plotlyOutput("traveltimes_dens")))
+               mainPanel(plotlyOutput("traveltimes_dens")))
     )
-))
-
+  ))
 
 
 
@@ -180,111 +166,97 @@ ui <- fluidPage(
 
 server <- function(input, output, session){
   # Tab:MBTA
-  # set up api_key first
-  api_key = "AIzaSyAZZEueevQyJrIRJUregAEbUC775LEfrxg"
-   output$map <- renderLeaflet({
-     
-    t_stop1 <- reactive({
-      coor1 <- red_travel_times %>% filter(dept_name == input$Tstop1) %>% select(dept_lon,dept_lat)
-      c(coor1[1],coor1[2])
-      })
-    t_stop2 <- reactive({
-      coor2 <- red_travel_times %>% filter(dept_name == input$Tstop1) %>% select(dept_lon,dept_lat)
-      c(coor2[1],coor2[2])
-    })
-    
-     doc=mp_directions(
-       origin = t_stop1,
-       destination = t_stop2,
-       alternatives = TRUE,
-       key = appi_key,
-       quiet = T
-     )
-     # doc = mp_matrix(
-     #   origins = locations,
-     #   destinations = locations,
-     #   # transit_mode =
-     #   # arrival_time = The desired time of arrival for transit directions, as POSIXct
-     #   # departure_time =
-     #   # avoid = "ferries" or "indoor"
-     #   key = api_key,
-     #   quiet = TRUE)
-     
-     # mp_get_matrix(doc, value = "duration_text")
-     r = mp_get_routes(doc)
-     
-     library(leaflet)
-     pal = colorFactor(palette = "Dark2", domain = t$alternative_id) #pal is short for pallette
-     leaflet() %>% 
-       addProviderTiles("CartoDB.DarkMatter") %>%
-       addPolylines(data = r, opacity = 1, weight = 7, color = ~pal(alternative_id), 
-                    # input for label is from r$distance_text)
-                    label = ~distance_text,
-                    labelOptions = labelOptions(noHide = TRUE))
-    })
-   
-    # # Boston view
-    # leaflet() %>% setView(lng = -71.0589, lat = 42.3601, zoom = 12) %>% addTiles() %>%
-    #   # # addPolylines(data = red_travel_times, ~dept_lon,~dept_lat, color = "red") %>% 
-      # addCircleMarkers(data = red_travel_times, ~dept_lon,~dept_lat,radius=1) })
-      # addPolylines(unique(greenb_travel_times$dept_lon)[1:5],unique(greenb_travel_times$dept_lat)[1:5],color="green")%>%
-      # addCircleMarkers(unique(greenb_travel_times$dept_lon)[1:5],unique(greenb_travel_times$dept_lat)[1:5],radius=1,popup = unique(greenb_travel_times$dept_name))[1:5]}) #%>%
-      # 
-      # addPolylines(unique(greenc_travel_times$dept_lon),unique(greenc_travel_times$dept_lat,color="green")) %>%
-      # addCircleMarkers(unique(greenc_travel_times$dept_lon),unique(greenc_travel_times$dept_lat),radius=1,popup = unique(greenc_travel_times$dept_name)) %>%
-      # 
-      # addPolylines(unique(greend_travel_times$dept_lon),unique(greend_travel_times$dept_lat),color="green") %>%
-      # addCircleMarkers(unique(greend_travel_times$dept_lon),unique(greend_travel_times$dept_lat),radius=1,popup = unique(greend_travel_times$dept_name)) %>%
-      
-      # addPolylines(unique(greene_travel_times$dept_lon),unique(greene_travel_times$dept_lat),color="green") %>%
-      # addCircleMarkers(unique(greene_travel_times$dept_lon),unique(greene_travel_times$dept_lat),radius=1,popup = unique(greene_travel_times$dept_name)) %>%
-      
-      # addPolylines(unique(orange_travel_times$dept_lon),unique(orange_travel_times$dept_lat,color="orange")) %>%
-      # addCircleMarkers(unique(orange_travel_times$dept_lon),unique(orange_travel_times$dept_lat),radius=1,popup = unique(orange_travel_times$dept_name))})
-
   
-    output$intro <- renderText({
-     "The app displays MBTA transit information and my exploratory analysis of it. Information used for visualization is extracted from hostrical records of MBTA services and from current google Directions API. 
+  output$map <- renderLeaflet({
+    # # Boston view
+    leaflet() %>% setView(lng = -71.0589, lat = 42.3601, zoom = 12) %>% addProviderTiles("CartoDB.Positron") %>% 
+      addPolylines(data = red_stops, lng = ~stop_lon,lat = ~stop_lat, color = "Red") %>% 
+      addCircleMarkers(data = red_stops,lng = ~stop_lon, lat = ~stop_lat,radius=2,popup = red_stops$stop_name) %>% 
+       
+      addPolylines(data = mattpan_stops, lng = ~stop_lon,lat = ~stop_lat, color = "Red") %>% 
+      addCircleMarkers(data = mattpan_stops,lng = ~stop_lon, lat = ~stop_lat,radius=2,popup = mattpan_stops$stop_name) %>% 
+      
+      addPolylines(data = blue_stops, lng = ~stop_lon,lat = ~stop_lat, color = "Blue") %>% 
+      addCircleMarkers(data = blue_stops,lng = ~stop_lon, lat = ~stop_lat,radius=2,popup = blue_stops$stop_name) %>% 
+      
+      addPolylines(data = orange_stops, lng = ~stop_lon,lat = ~stop_lat, color = "Orange") %>% 
+      addCircleMarkers(data = orange_stops,lng = ~stop_lon, lat = ~stop_lat,radius=2,popup = orange_stops$stop_name) %>% 
+      
+      addPolylines(data = greenb_stops, lng = ~stop_lon,lat = ~stop_lat, color = "Green") %>% 
+      addCircleMarkers(data = greenb_stops,lng = ~stop_lon, lat = ~stop_lat,radius=2,popup = greenb_stops$stop_name) %>% 
+      
+      addPolylines(data = greenc_stops, lng = ~stop_lon,lat = ~stop_lat, color = "Green") %>% 
+      addCircleMarkers(data = greenc_stops,lng = ~stop_lon, lat = ~stop_lat,radius=2,popup = greenc_stops$stop_name) %>% 
+      
+      addPolylines(data = greend_stops, lng = ~stop_lon,lat = ~stop_lat, color = "Green") %>% 
+      addCircleMarkers(data = greend_stops,lng = ~stop_lon, lat = ~stop_lat,radius=2,popup = greend_stops$stop_name) %>% 
+      
+      addPolylines(data = greene_stops, lng = ~stop_lon,lat = ~stop_lat, color = "green") %>% 
+      addCircleMarkers(data = greene_stops,lng = ~stop_lon, lat = ~stop_lat,radius=2,popup = greene_stops$stop_name) 
+     })
+  
+  
+  output$intro <- renderText({
+    "The app displays MBTA transit information and my exploratory analysis of it. Information used for visualization is extracted from hostrical records of MBTA services and from current google Directions API. 
       I selected data from Novemenber 2021 to October 2022 to explore."})
-    
+  
   # Tab bus
-    
-    output$bus_table <- renderDT({
-      sl_routes_traveltimes %>% filter(stop_name == input$bus_stop) %>% dplyr::select(route_id,direction_id, headway, scheduled, actual) 
-    })
-    
+  
+  output$bus_table <- renderDT({
+    sl_routes_traveltimes %>% filter(stop_name == input$bus_stop) %>% dplyr::select(route_id,direction_id, headway, scheduled, actual) 
+  })
+  
   # output$SLboxplot2 <- renderPlotly(
   #   plot_ly(data = sl_routes_traveltimes, y=~headway, color = ~stop_name, type = "box") %>%
   #     layout(title="Box plot of silver line travel times by stop", yaxis=list(title="Time",range=c(0,1000)))
   # )
   # 
-    
+  
   # Tab:  Ferry
-    output$boat_table <- renderDT({
-      boat_trips_may %>% filter(stop_name == input$boat_stop) %>% dplyr::select(trip_headsign,arrival_time, departure_time, direction_destinaion)}) 
+  output$boat_table <- renderDT({
+    boat_trips_may %>% filter(stop_name == input$boat_stop) %>% dplyr::select(trip_headsign,arrival_time, departure_time, direction_destination)}) 
+  
+  # Tab:rapid transit 
+  
+
+  updateSelectizeInput(session, 'Tstop1', choices = red_stops$stop_name, server = TRUE)
+  updateSelectizeInput(session, 'Tstop2', choices = red_stops$stop_name, server = TRUE)
+  
+  
+  
+  output$subwaymap <- renderLeaflet({
+    doc=mp_directions(
+      origin = input$Tstop1,
+      destination = input$Tstop2,
+      mode = "transit",
+      transit_mode = "subway",
+      alternatives = TRUE,
+      key = appi_key,
+      quiet = T)
+    r = mp_get_routes(doc)
     
+    pal = colorFactor(palette = "Dark2", domain = t$alternative_id) 
     
-   # Tab:rapid transit:
+    leaflet() %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addPolylines(data = r, opacity = 1, weight = 7, color = ~pal(alternative_id),
+                   # input for label is from r$distance_text)
+                   label = ~duration_text,
+                   labelOptions = labelOptions(noHide = TRUE))
+  })
+  
+  # Tab:rapid transit EDA
   tdirection <- reactive({
-   input$lines })
+    input$lines })
   
   output$traveltimes_dens <- renderPlotly(
-    # if (subwayline() == "Red Line"){
-    # # Northbound density plot
-    # red_n<- plot_ly(red_to_north_df, x = ~x, y = ~y, color = ~dept_name)%>% add_lines() %>% 
-    #   layout(title = "Northbound density plot by stop", yaxis=list(title="Density"), xaxis=list(title="Northbound Travel Times",range = c(0,4000)))
-    # 
-    # # Southbound density plot
-    # # red_s <- plot_ly(red_to_south_df, x = ~x, y = ~y, color = ~dept_name)%>% add_lines() %>% 
-    # #   layout(title = "Southbound density plot by stop", yaxis=list(title="Density"), xaxis=list(title="SouthBound Travel Times",range = c(0,2000)))
-    # }else{
-  
+    
     if(tdirection() == "Northbound"){
-    org_n <- plot_ly(orange_to_north_df, x = ~x, y = ~y, color = ~dept_name)%>% add_lines() %>% 
+      org_n <- plot_ly(orange_to_north_df, x = ~x, y = ~y, color = ~dept_name)%>% add_lines() %>% 
         layout(title = "Northbound density plot by stop", yaxis=list(title="Density"), xaxis=list(title="Northbound Travel Times",range = c(0,3000)))
     }
     else{
-    org_s <- plot_ly(orange_to_south_df, x = ~x, y = ~y, color = ~dept_name)%>% add_lines() %>% 
+      org_s <- plot_ly(orange_to_south_df, x = ~x, y = ~y, color = ~dept_name)%>% add_lines() %>% 
         layout(title = "Southbound density plot by stop", yaxis=list(title="Density"), xaxis=list(title="SouthBound Travel Times",range = c(0,2000)))
     }
   )
@@ -292,3 +264,8 @@ server <- function(input, output, session){
 }
 
 shinyApp(ui, server)
+
+
+
+
+
